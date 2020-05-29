704ad4e475ddfe58a116b3ed83a398790568c076
Alexsandr <guravskiy79@gmail.com>
2020-05-29 20:58:58 +0300

    corrected the type of chat

--- src/main/resources/js/router/router.js
+++ src/main/resources/js/router/router.js
@@ -1,29 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Login from '../pages/Login.vue'
-import Registration from '../pages/Registration.vue'
-import ForgotPassword from '../pages/ForgotPassword.vue'
-import Profile from '../pages/Profile.vue'
-import Settings from '../pages/Settings.vue'
-import Search from '../pages/Search.vue'
-import Message from '../pages/Message.vue'
-import PageViews from "../pages/PageViews.vue";
-import Sympathy from "../pages/Sympathy.vue";
-import NotFoundPage from "../pages/NotFoundPage.vue";
-import AdminPanel from "../pages/AdminPanel.vue";
+// import Login from '../pages/Login.vue'
+// import Registration from '../pages/Registration.vue'
+// import ForgotPassword from '../pages/ForgotPassword.vue'
+// import Profile from '../pages/Profile.vue'
+// import Settings from '../pages/Settings.vue'
+// import Search from '../pages/Search.vue'
+// import Message from '../pages/Message.vue'
+// import PageViews from "../pages/PageViews.vue";
+// import Sympathy from "../pages/Sympathy.vue";
+// import NotFoundPage from "../pages/NotFoundPage.vue";
+// import AdminPanel from "../pages/AdminPanel.vue";
 
-// const Login = () => import('../pages/Login.vue')
-// const Registration  = () => import( '../pages/Registration.vue')
-// const ForgotPassword  = () => import( '../pages/ForgotPassword.vue')
-// const Profile  = () => import( '../pages/Profile.vue')
-// const Settings  = () => import( '../pages/Settings.vue')
-// const Search  = () => import( '../pages/Search.vue')
-// const Message  = () => import( '../pages/Message.vue')
-// const PageViews  = () => import( "../pages/PageViews.vue")
-// const Sympathy  = () => import( "../pages/Sympathy.vue")
-// const NotFoundPage  = () => import( "../pages/NotFoundPage.vue")
-// const AdminPanel  = () => import( "../pages/AdminPanel.vue")
+const Login = () => import('../pages/Login.vue')
+const Registration  = () => import( '../pages/Registration.vue')
+const ForgotPassword  = () => import( '../pages/ForgotPassword.vue')
+const Profile  = () => import( '../pages/Profile.vue')
+const Settings  = () => import( '../pages/Settings.vue')
+const Search  = () => import( '../pages/Search.vue')
+const Message  = () => import( '../pages/Message.vue')
+const PageViews  = () => import( "../pages/PageViews.vue")
+const Sympathy  = () => import( "../pages/Sympathy.vue")
+const NotFoundPage  = () => import( "../pages/NotFoundPage.vue")
+const AdminPanel  = () => import( "../pages/AdminPanel.vue")
 
 Vue.use(VueRouter);
 
